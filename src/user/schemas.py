a34fd696fcbbe4364f7b04d1e57bd1eb9b375779
zahra <zh.dehghani741@gmail.com>
2024-08-23 20:58:46 +0330

    add validator

--- src/user/schemas.py
+++ src/user/schemas.py
@@ -1,19 +1,47 @@
-from pydantic import BaseModel
+import re
+
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class UserSchema(BaseModel):
-    email: str
-    first_name: str
-    last_name: str
-    password: str
+    email: EmailStr = Field(pattern=r".+@example\.com$")
+    first_name: str = Field(min_length=2, frozen=True)
+    last_name: str = Field(min_length=2, frozen=True)
+    password: str = Field(..., min_length=8, max_length=64)
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        if not re.search(r"\d", value):
+            raise ValueError("Password must contain at least one digit.")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[\W]", value):
+            raise ValueError("Password must contain at least one special character.")
+        return value
 
 
-class UserSerialiser(UserSchema):
+class UserSerializer(UserSchema):
     class Config:
         from_attributes = True
 
 
 class UserSchemaUpdate(BaseModel):
-    first_name: str
-    last_name: str
-    password: str
+    first_name: str = Field(min_length=2, frozen=True)
+    last_name: str = Field(min_length=2, frozen=True)
+    password: str = Field(..., min_length=8, max_length=64)
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        if not re.search(r"\d", value):
+            raise ValueError("Password must contain at least one digit.")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[\W]", value):
+            raise ValueError("Password must contain at least one special character.")
+        return value
